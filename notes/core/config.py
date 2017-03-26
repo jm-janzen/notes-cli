ce102be82807ae8b0b2ecbcb1ae02af169fda9c8
JM <jmjanzen@gmail.com>
2017-03-26 12:49:01 -0400

    Move singleton class to own dir

--- notes/core/config.py
+++ notes/core/config.py
@@ -1,14 +1,7 @@
 import os
 
 from configparser import ConfigParser
-
-
-class Singleton(type):
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
+from . utils.singleton import Singleton
 
 
 class Config(metaclass=Singleton):
